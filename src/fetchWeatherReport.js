3d4341aac0f938a0032691021a0fa336363f3f96
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-11-29 21:57:07 -0500

    Create separate file for fetching weather report

---
+++ src/fetchWeatherReport.js
@@ -0,0 +1,102 @@
+import API_KEY from "./apiKey";
+
+function buildForecastWeatherURL(location, numDays) {
+  return `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&days=${numDays}`;
+}
+
+async function fetchWeatherForecast(location, numDays) {
+  const forecastWeatherURL = buildForecastWeatherURL(location, numDays);
+  const forecastWeatherReportJSON = await fetch(forecastWeatherURL, {
+    mode: "cors",
+  });
+  const forecastWeatherReport = await forecastWeatherReportJSON.json();
+
+  return forecastWeatherReport;
+}
+
+function extractCurrentWeatherData(weatherForecast) {
+  const currentWeather = weatherForecast.current;
+  const tempF = currentWeather.temp_f;
+  const tempC = currentWeather.temp_c;
+  const feelsLikeF = currentWeather.feelslike_f;
+  const feelsLikeC = currentWeather.feelslike_c;
+  const windSpeedMPH = currentWeather.wind_mph;
+  const windGustMPH = currentWeather.gust_mph;
+  const windDirection = currentWeather.wind_dir;
+  const UVIndex = currentWeather.uv;
+  const conditionText = currentWeather.condition.text;
+  const conditionIconURL = currentWeather.condition.icon;
+  const { humidity } = currentWeather;
+  return {
+    tempF,
+    tempC,
+    feelsLikeF,
+    feelsLikeC,
+    windSpeedMPH,
+    windGustMPH,
+    windDirection,
+    UVIndex,
+    conditionText,
+    conditionIconURL,
+    humidity,
+  };
+}
+
+function extractLocationData(weatherForecast) {
+  const locationData = weatherForecast.location;
+  const { country, name, region } = locationData;
+  return {
+    country,
+    name,
+    region,
+  };
+}
+
+function extractFutureWeather(weatherForecast, daysAhead) {
+  const weatherForDayAhead = weatherForecast.forecast.forecastday[daysAhead];
+  const moonPhase = weatherForDayAhead.astro.moon_phase;
+  const { moonrise, moonset, sunrise, sunset } = weatherForDayAhead.astro;
+  const { date } = weatherForDayAhead;
+  const humidity = weatherForDayAhead.day.avghumidity;
+  const maxTempF = weatherForDayAhead.day.maxtemp_f;
+  const maxTempC = weatherForDayAhead.day.maxtemp_c;
+  const minTempF = weatherForDayAhead.day.mintemp_f;
+  const minTempC = weatherForDayAhead.day.mintemp_c;
+  const conditionText = weatherForDayAhead.day.condition.text;
+  const conditionIconURL = weatherForDayAhead.day.condition.icon;
+  const UVIndex = weatherForDayAhead.day.uv;
+  const maxWindMPH = weatherForDayAhead.day.maxwind_mph;
+  return {
+    moonPhase,
+    moonrise,
+    moonset,
+    sunrise,
+    sunset,
+    date,
+    humidity,
+    maxTempF,
+    maxTempC,
+    minTempF,
+    minTempC,
+    conditionText,
+    conditionIconURL,
+    UVIndex,
+    maxWindMPH,
+  };
+}
+
+export default async function fetchWeatherReport(location, numDays) {
+  const weatherForecast = await fetchWeatherForecast(location, numDays);
+  const currentWeather = extractCurrentWeatherData(weatherForecast);
+  const locationData = extractLocationData(weatherForecast);
+  const weatherForecastArray = [];
+  for (let dayAhead = 0; dayAhead < numDays; dayAhead += 1) {
+    const daysWeather = extractFutureWeather(weatherForecast, dayAhead);
+    weatherForecastArray.push(daysWeather);
+  }
+  return {
+    currentWeather,
+    locationData,
+    weatherForecastArray,
+  };
+}
