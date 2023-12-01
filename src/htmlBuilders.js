b39956119a933adcc3fc87d3ab8f4884e77b5316
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-12-01 10:45:53 -0500

    Create Html builders for page elements

---
+++ src/htmlBuilders.js
@@ -0,0 +1,165 @@
+import tempIcon from "./img/thermometer.svg";
+import windIcon from "./img/windsock.svg";
+import humidityIcon from "./img/water-percent.svg";
+import uvIcon from "./img/sun-wireless-outline.svg";
+import conditionLoadIcon from "./img/dots-circle.svg";
+
+const degSymbol = "\u{00B0}";
+
+export function wrapHtmlElements(wrapperTag, ...elements) {
+  const wrapperHtml = document.createElement(wrapperTag);
+  wrapperHtml.append(...elements);
+  return wrapperHtml;
+}
+
+export function buildIconHtml(icon) {
+  const iconHtml = document.createElement("img");
+  iconHtml.src = icon;
+  return iconHtml;
+}
+
+export function buildTextHtml(text) {
+  const textHtml = document.createElement("p");
+  textHtml.innerText = text;
+  return textHtml;
+}
+
+export function buildLabelHtml(inputId, text) {
+  const labelHtml = document.createElement("label");
+  labelHtml.for = inputId;
+  labelHtml.innerText = text;
+  return labelHtml;
+}
+
+export function buildInputHtml(type, name, id) {
+  const inputHtml = document.createElement("input");
+  inputHtml.type = type;
+  inputHtml.name = name;
+  if (id) inputHtml.id = id;
+  return inputHtml;
+}
+
+export function buildHeaderTextHtml(headerText, headerLevel) {
+  const headerTextHtml = document.createElement(`h${headerLevel}`);
+  headerTextHtml.innerHTML = headerText;
+  return headerTextHtml;
+}
+
+export function buildSelectOption(text, value) {
+  const optionHtml = document.createElement("option");
+  optionHtml.value = value;
+  optionHtml.innerText = text;
+  return optionHtml;
+}
+
+export function buildCurrentWeatherHtml() {
+  const currentConditionIconHtml = buildIconHtml(conditionLoadIcon);
+  currentConditionIconHtml.id = "current-condition-icon";
+  currentConditionIconHtml.classList.add("condition-icon");
+  const currentConditionTextHtml = buildTextHtml("---");
+  currentConditionTextHtml.id = "current-condition-text";
+
+  const tempIconHtml = buildIconHtml(tempIcon);
+  tempIconHtml.classList.add("static-icon");
+  const currentTempTextHtml = buildTextHtml(`--${degSymbol}F`);
+  currentTempTextHtml.id = "current-temp";
+  const currentFeelsLikeLabelHtml = buildTextHtml("Feels like");
+  currentFeelsLikeLabelHtml.id = "feels-like-label";
+  const currentFeelsLikeTextHtml = buildTextHtml(`--${degSymbol}F`);
+  currentFeelsLikeTextHtml.id = "current-feels-like";
+
+  const currentWindIconHtml = buildIconHtml(windIcon);
+  currentWindIconHtml.classList.add("static-icon");
+  const currentWindSpeedTextHtml = buildTextHtml("-- mph");
+  currentWindSpeedTextHtml.id = "current-wind-speed";
+  const currentWindDirectionHtml = buildTextHtml("---");
+  currentWindDirectionHtml.id = "current-wind-direction";
+
+  const currentUVIndexIconHtml = buildIconHtml(uvIcon);
+  currentUVIndexIconHtml.classList.add("static-icon");
+  const currentUVIndexTextHtml = buildTextHtml("---");
+  currentUVIndexTextHtml.id = "current-uv-index";
+
+  const currentHumidityIconHtml = buildIconHtml(humidityIcon);
+  currentHumidityIconHtml.classList.add("static-icon");
+  const currentHumidityTextHtml = buildTextHtml("-- %");
+  currentHumidityTextHtml.id = "current-humidity";
+
+  const currentPOPLabelHtml = buildTextHtml("POP");
+  currentPOPLabelHtml.classList.add("static-label");
+  const currentPOPTextHtml = buildTextHtml("-- %");
+  currentPOPTextHtml.id = "current-pop";
+
+  const cityTextHtml = buildTextHtml("---,");
+  cityTextHtml.id = "location-city";
+  const countryTextHtml = buildTextHtml("---");
+  countryTextHtml.id = "location-country";
+
+  const currentWeatherHtml = wrapHtmlElements(
+    "div",
+    currentConditionIconHtml,
+    currentConditionTextHtml,
+    tempIconHtml,
+    currentTempTextHtml,
+    currentFeelsLikeLabelHtml,
+    currentFeelsLikeTextHtml,
+    currentWindIconHtml,
+    currentWindSpeedTextHtml,
+    currentWindDirectionHtml,
+    currentUVIndexIconHtml,
+    currentUVIndexTextHtml,
+    currentHumidityIconHtml,
+    currentHumidityTextHtml,
+    currentPOPLabelHtml,
+    currentPOPTextHtml,
+    cityTextHtml,
+    countryTextHtml
+  );
+  currentWeatherHtml.id = "current-weather-container";
+
+  return currentWeatherHtml;
+}
+
+function buildForecastCardHtml() {
+  const dayNameTextHtml = buildTextHtml("---");
+  dayNameTextHtml.classList.add("day-name");
+
+  const highTempTextHtml = buildTextHtml(`--${degSymbol}F`);
+  highTempTextHtml.classList.add("temp-high");
+
+  const lowTempTextHtml = buildTextHtml(`--${degSymbol}F`);
+  lowTempTextHtml.classList.add("temp-low");
+
+  const conditionIconHtml = buildIconHtml(conditionLoadIcon);
+  conditionIconHtml.classList.add("condition-icon");
+
+  const forecastCardHtml = wrapHtmlElements(
+    "div",
+    dayNameTextHtml,
+    highTempTextHtml,
+    lowTempTextHtml,
+    conditionIconHtml
+  );
+  forecastCardHtml.classList.add("forecast-card");
+
+  return forecastCardHtml;
+}
+
+export function buildThreeDayForecastHtml() {
+  const todayForecastCardHtml = buildForecastCardHtml();
+  todayForecastCardHtml.id = "forecast-today";
+  const tomorrowForecastCardHtml = buildForecastCardHtml();
+  tomorrowForecastCardHtml.id = "forecast-tomorrow";
+  const thirdForecastCardHtml = buildForecastCardHtml();
+  thirdForecastCardHtml.id = "forecast-third";
+
+  const threeDayForecastHtml = wrapHtmlElements(
+    "div",
+    todayForecastCardHtml,
+    tomorrowForecastCardHtml,
+    thirdForecastCardHtml
+  );
+  threeDayForecastHtml.id = "three-day-forecast-container";
+
+  return threeDayForecastHtml;
+}
