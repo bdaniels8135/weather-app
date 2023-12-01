4c5afd00c289d7a9e31bee507e9dab1d237926f1
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-12-01 10:46:47 -0500

    Refactor index to build a basic page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import "./index.css";
 import fetchWeatherReport from "./fetchWeatherReport";
+import {
+  buildInputHtml,
+  buildCurrentWeatherHtml,
+  buildThreeDayForecastHtml,
+} from "./htmlBuilders";
 
-const weatherReportPromise = fetchWeatherReport("08081", 4);
+const degSymbol = "\u{00B0}";
 
-weatherReportPromise
-  .then((weatherReport) => {
-    console.log(weatherReport);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// const weatherReportPromise = fetchWeatherReport("08081", 4);
+
+// weatherReportPromise
+//   .then((weatherReport) => {
+//     console.log(weatherReport);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+const searchHtml = buildInputHtml("search", "search-input", "search-input");
+
+const tempToggleHtml = buildInputHtml(
+  "button",
+  "temp-toggle-btn",
+  "temp-toggle-btn"
+);
+tempToggleHtml.value = `${degSymbol}F/${degSymbol}C`; // Degree Symbols
+
+const currentWeatherHtml = buildCurrentWeatherHtml();
+
+const threeDayForecastHtml = buildThreeDayForecastHtml();
+
+const body = document.querySelector("body");
+
+body.append(
+  searchHtml,
+  tempToggleHtml,
+  currentWeatherHtml,
+  threeDayForecastHtml
+);
