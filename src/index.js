4dcbffa4d39e8329c7ba058be6f591c91b92b0ed
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-12-01 20:17:27 -0500

    Refactor index to actually fetch and display data

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import "./index.css";
-// import fetchWeatherReport from "./fetchWeatherReport";
-import buildPageElements from "./buildPageElements";
-
-// const weatherReportPromise = fetchWeatherReport("08081", 3);
-
-// weatherReportPromise
-//   .then((weatherReport) => {
-//     console.log(weatherReport);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
+import fetchWeatherReport from "./fetchWeatherReport";
+import buildPageMainHtml from "./buildPageMainHtml";
+import displayWeatherReport from "./displayWeatherReport";
 
 const body = document.querySelector("body");
-body.append(buildPageElements());
+const main = buildPageMainHtml();
+body.append(main);
+
+const weatherReportPromise = fetchWeatherReport("08081", 3);
+
+weatherReportPromise
+  .then((weatherReport) => {
+    displayWeatherReport(weatherReport);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
